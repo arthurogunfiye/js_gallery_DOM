591609c258518596eabbb9fd279831c185c1eb59
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-05-01 07:07:46 +0100

    make build

---
+++ index.29785fd1.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.addEventListener("click",function(t){if("img"===t.target.tagName?a=t.target:"a"===t.target.tagName&&(a=t.target.querySelector("img")),a){var a,r=a.closest("a");r&&(t.preventDefault(),e.src=r.href)}});
+//# sourceMappingURL=index.29785fd1.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const mainImage = document.querySelector("#largeImg");
+document.addEventListener("click", function(e) {
+    let clickedImg;
+    if (e.target.tagName === "img") clickedImg = e.target;
+    else if (e.target.tagName === "a") clickedImg = e.target.querySelector("img");
+    if (!clickedImg) return;
+    const link = clickedImg.closest("a");
+    if (link) {
+        e.preventDefault();
+        mainImage.src = link.href;
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
