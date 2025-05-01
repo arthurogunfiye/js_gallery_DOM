88e019ed2cf2387fcb1a2e5b9b2ba6f9285a0838
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-05-01 07:07:26 +0100

    Gallery DOM solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const mainImage = document.querySelector('#largeImg');
+
+document.addEventListener('click', function (e) {
+  let clickedImg;
+
+  if (e.target.tagName === 'img') {
+    clickedImg = e.target;
+  } else if (e.target.tagName === 'a') {
+    clickedImg = e.target.querySelector('img');
+  }
+
+  if (!clickedImg) {
+    return;
+  }
+
+  const link = clickedImg.closest('a');
+
+  if (link) {
+    e.preventDefault();
+    mainImage.src = link.href;
+  }
+});
